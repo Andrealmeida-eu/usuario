b5c006f88d54521167959a9b05f7372d7c1b7f07
André Almeida <a.luiz.almeida03@outlook.com>
2025-03-04 16:29:32 -0300

    adicionado metodo que salva dados do Usuario

--- src/main/java/com/ajsolucoes/infrastructure/entity/Telefone.java
+++ src/main/java/com/ajsolucoes/infrastructure/entity/Telefone.java
@@ -1,10 +1,7 @@
 package com.ajsolucoes.infrastructure.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
@@ -13,6 +10,7 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "telefones")
+@Builder
 public class Telefone {
 
     @Id
