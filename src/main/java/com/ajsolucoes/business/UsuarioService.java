609fdd2958c80d000c18eaf237e8357b7e27a9fc
André Almeida <a.luiz.almeida03@outlook.com>
2025-03-05 12:59:13 -0300

    inclusao de metodo put de usuario

--- src/main/java/com/ajsolucoes/business/UsuarioService.java
+++ src/main/java/com/ajsolucoes/business/UsuarioService.java
@@ -7,6 +7,7 @@ import com.ajsolucoes.infrastructure.entity.Usuario;
 import com.ajsolucoes.infrastructure.exceptions.ConflitException;
 import com.ajsolucoes.infrastructure.exceptions.ResourceNotfoundException;
 import com.ajsolucoes.infrastructure.repository.UsuarioRepository;
+import com.ajsolucoes.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,11 +18,13 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
-    private final PasswordEncoder PasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
 
 public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
     emailExiste(usuarioDTO.getEmail());
-    usuarioDTO.setSenha(PasswordEncoder.encode(usuarioDTO.getSenha()));
+    usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
     Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
     return usuarioConverter.paraUsuarioDTO(
             usuarioRepository.save(usuario));
@@ -49,5 +52,28 @@ public void emailExiste(String email) {
 
     public void deletaUsuarioPorEmail(String email){
         usuarioRepository.deleteByEmail(email);
+    }
+
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto){
+
+        //buscamos o email do usuario atraves do token (tirar a obrigatoriedade do email)
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
+
+        //criptografia na senha
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
+
+        //Buscamos os dados no banco
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(() ->
+                new ResourceNotfoundException("Email nao encontrado"));
+
+        //Mesclou os dados que recebemos dto com os dados do banco
+        Usuario usuario = usuarioConverter.updatedUsuario(dto, usuarioEntity);
+
+
+
+        //salvou os dados do usuario convertido e depois converteu o retorno para dto
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+
+
     }
 }
